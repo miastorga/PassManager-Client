8f8b66eb153e2e9b66c3c67a8604118d34939db9
Miguel Astorga <86029365+miastorga@users.noreply.github.com>
2024-07-20 16:53:39 -0400

    copiar user y pass, dialog edit y delte sasfe-box

--- src/app/vault/safe-box/safe-box.component.ts
+++ src/app/vault/safe-box/safe-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, TrackByFunction, computed, effect, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import {
   HlmCardContentDirective,
   HlmCardDescriptionDirective,
@@ -7,10 +7,26 @@ import {
   HlmCardHeaderDirective,
   HlmCardTitleDirective,
 } from '@spartan-ng/ui-card-helm';
-
+import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { provideIcons } from '@ng-icons/core';
+import { lucideTrash2, lucideEdit, lucideKey, lucideUser2 } from '@ng-icons/lucide';
+import {
+  HlmDialogComponent,
+  HlmDialogContentComponent,
+  HlmDialogDescriptionDirective,
+  HlmDialogFooterComponent,
+  HlmDialogHeaderComponent,
+  HlmDialogTitleDirective,
+} from '@spartan-ng/ui-dialog-helm';
+import { Clipboard } from "@angular/cdk/clipboard"
+import { BrnDialogContentDirective, BrnDialogTriggerDirective } from '@spartan-ng/ui-dialog-brain';
+import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
+import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
 @Component({
   selector: 'app-safe-box',
   standalone: true,
+  providers: [provideIcons({ lucideTrash2, lucideEdit, lucideKey, lucideUser2 })],
   imports: [
     HlmCardContentDirective,
     HlmCardDescriptionDirective,
@@ -18,9 +34,34 @@ import {
     HlmCardFooterDirective,
     HlmCardHeaderDirective,
     HlmCardTitleDirective,
+    HlmIconComponent,
+    HlmButtonDirective,
+    HlmDialogComponent,
+    HlmDialogContentComponent,
+    HlmDialogHeaderComponent,
+    HlmDialogFooterComponent,
+    HlmDialogTitleDirective,
+    HlmDialogDescriptionDirective,
+    BrnDialogTriggerDirective,
+    BrnDialogContentDirective,
+    HlmLabelDirective,
+    HlmInputDirective,
+    HlmButtonDirective,
+
   ],
   templateUrl: './safe-box.component.html',
   styleUrl: './safe-box.component.css'
 })
 export class SafeBoxComponent {
+
+  usuario = '20.568.823-8'
+  password = '*********'
+
+  constructor(private clipboard: Clipboard) {
+
+  }
+
+  copyToClipBoard(content: string) {
+    this.clipboard.copy(content)
+  }
 }
--- src/app/vault/vault-layout/header/header.component.ts
+++ src/app/vault/vault-layout/header/header.component.ts
@@ -1,12 +1,73 @@
 import { Component } from '@angular/core';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+
+import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
+import { provideIcons } from '@ng-icons/core';
+import { lucidePlus } from '@ng-icons/lucide';
+import { BrnDialogContentDirective, BrnDialogTriggerDirective } from '@spartan-ng/ui-dialog-brain';
+import {
+  HlmDialogComponent,
+  HlmDialogContentComponent,
+  HlmDialogDescriptionDirective,
+  HlmDialogFooterComponent,
+  HlmDialogHeaderComponent,
+  HlmDialogTitleDirective,
+} from '@spartan-ng/ui-dialog-helm';
+import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
+import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, Form } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  providers: [provideIcons({ lucidePlus })],
+  imports: [
+    HlmIconComponent,
+    HlmDialogComponent,
+    HlmDialogContentComponent,
+    HlmDialogHeaderComponent,
+    HlmDialogFooterComponent,
+    HlmDialogTitleDirective,
+    HlmDialogDescriptionDirective,
+
+    HlmLabelDirective,
+    HlmInputDirective,
+    HlmButtonDirective,
+
+    BrnDialogTriggerDirective,
+    BrnDialogContentDirective,
+    ReactiveFormsModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  safeBoxForm: FormGroup;
+  folderForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.safeBoxForm = this.formBuilder.group({
+      nombre: ['', [Validators.required]],
+      carpeta: [''],
+      usuario: ['', [Validators.required]],
+      contraseña: ['', [Validators.required]],
+      notas: ['', [Validators.required, Validators.maxLength(500)]]
+    })
+
+    this.folderForm = this.formBuilder.group({
+      carpeta: ['', [Validators.required]]
+    })
+  }
+
+  onSubmit() {
+    console.warn(this.safeBoxForm.value);
+  }
+
+  onSubmitFolder() {
+    console.warn(this.folderForm.value);
+  }
+
+  hasErrors(controlName: string, errorType: string) {
+    return this.safeBoxForm.get(controlName)?.hasError(errorType) && this.safeBoxForm.get(controlName)?.touched;
+  }
 }
